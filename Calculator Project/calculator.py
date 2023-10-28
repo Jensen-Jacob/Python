1f7ebbe3c7a9a47d0eaacad7c1696fd5d882b81c
Jensen-Jacob <jensenjacobgeorge@gmail.com>
2023-10-28 12:58:26 +0530

    Update 'guess-the-number' to minimize user errors

--- Calculator Project/calculator.py
+++ Calculator Project/calculator.py
@@ -1,74 +1,74 @@
+#Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player.
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
 from art import logo
-print(logo)
+import random
 
-# Add function
-def add(a, b):
-  return a + b
+# print(logo)
 
-# Subtract function
-def subtract(a, b):
-  return a - b
 
-# Multiply function
-def multiply(a, b):
-  return a * b
-
-# Divide function
-def divide(a, b):
-  return a / b
-
-operations = {
-  "+": add,
-  "-": subtract,
-  "*": multiply,
-  "/": divide
-}
-
-continue_calculation = False
-
-print("Welcome to the calculator!")
-
-first_number = 0
-second_number = 0
-
-def calculator():
-    global continue_calculation
-    global first_number
-    global second_number
+def play():
+    print("Welcome to the number guessing game!")
+    number = random.randint(1, 100)
+    game_over = False
     while True:
         try:
-            if not continue_calculation:
-                first_number = float(input("What's the first number? "))
-            second_number = float(input("What's the second number? "))
-            break
-        except:
-            print("Invalid input! Try again")
-    print("What operation do you want to perform?")
-    while True:
-        try:
-            for key in operations:
-                print(key)
-            choice = input("Enter operation: ")
-            if choice in operations:
-                result = operations[choice](first_number, second_number)
-                print(f"{first_number} {choice} {second_number} = {result}")
+            difficulty = input(
+                "Choose a difficulty: Type 'easy' or 'hard': ").lower()
+            if difficulty != "easy" and difficulty != "hard":
+                raise Exception("Invalid input!")
             else:
-                raise Exception()
-            break
-        except:
-            print(f"'{choice}' is not a valid operation! Please try again")
-    while True:
-        try:
-            continue_calculation_choice = input(f"Type 'y' to continue calculation with {result} or 'n' to start a new calculation: ")
-            if continue_calculation_choice == "y":
-                continue_calculation = True
-                first_number = result
                 break
+        except Exception as e:
+            print(f"An error ocurred: {e}, try again!")
+    if difficulty == "easy":
+        turns = 10
+    else:
+        turns = 5
+    while not game_over:
+        print(f"You have {turns} turns to guess the number.")
+        while True:
+            try:
+                guess = int(input("Make a guess: "))
+                if type(guess) == int:
+                    break
+            except:
+                print("Invalid Input! Try again.")
+        if guess < number:
+            print("Too low.")
+            turns -= 1
+        elif guess > number:
+            print("Too high.")
+            turns -= 1
+        else:
+            print(f"You got it! The number was {number}.")
+            game_over = True
+        if turns == 0:
+            print(f"Game over! The number was {number}.")
+            game_over = True
+    while True:
+        try:
+            play_again = input(
+                "Would you like to play again? Type 'yes' or 'no': ").lower()
+            if play_again != "yes" and play_again != "no":
+                raise Exception(
+                    "Please answer 'yes' to play again and 'no' to exit.")
             else:
-                first_number = 0
-                second_number = 0
-        except:
-            print("Invalid input! Try again.")
-    calculator()
-    
-calculator()
+                break
+        except Exception as e:
+            print(f"{e}")
+
+    if play_again == "yes":
+        play()
+    else:
+        print("Thankyou for playing!")
+
+
+play()
